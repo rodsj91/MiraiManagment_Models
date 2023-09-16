1c2ba471e77cb22319a6c2e3eb9344788501be45
Rodrigo Jimenez <r.jmnz91@gmail.com>
2023-09-16 12:13:28 -0600

    [RJ] Modelo Login

---
+++ MiraiManagment_Models/Login/LoginModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiraiManagment_Models.LoginModels
+{
+    public class LoginModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public LoginModel(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+    }
+}
